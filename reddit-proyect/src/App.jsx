d5851b4a8251ce89e065c3d6729b416902be1461
martin gaido <martingaido00@gmail.com>
2024-03-19 07:33:18 -0300

    se agrego el menu de la izquierda

--- reddit-proyect/src/App.jsx
+++ reddit-proyect/src/App.jsx
@@ -1,13 +1,18 @@
+import "./style/styleApp.css"
 import { ComponentComunidades } from "./components/ComponentComunidades "
 import { ComponentDestacados } from "./components/ComponentDestacados"
 import { ComponentFiltro } from "./components/ComponentFiltro"
 import { ComponentHeader } from "./components/ComponentHeader"
 import { ComponentPanelIzquierdo } from "./components/ComponentPanelIzquierdo"
 import { ComponentPrincipales } from "./components/ComponentPrincipales"
-
 export const App = () => {
   
   return (
+    <>
+    <ComponentPanelIzquierdo />
+    <div className="container__Principal">
     <ComponentPrincipales community={'Rubiu5'} time={1710681711673} tittle={'Quiero aprender SQL pero no se que sgbd usar'} description={'Hay varias opciones pero me gustaría alguna que sea solicitada en el mercado y sobretodo que sea gratis para aprender'} />
+    </div>
+    </>
   )
 }
--- reddit-proyect/src/components/ComponentPanelIzquierdo.jsx
+++ reddit-proyect/src/components/ComponentPanelIzquierdo.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
 import { ComponentTEMAS } from './ComponentTEMAS'
 import { ComponentRECURSOS } from './ComponentRECURSOS'
+import "../style/stylePanelIzquierdo.css"
 
 
 export const ComponentPanelIzquierdo = () => {
   return (
-    <>
-    <div>ComponentPanelIzquierdo</div>
-    <ComponentTEMAS />
-    <ComponentRECURSOS  />
-    </>
+    <div className='container__panel'>
+
+      <div className='boton__Principal'>
+        <a>
+
+          <i class="fa-solid fa-house"></i>
+          <label className="Principal">
+            Principal
+          </label>
+        </a>
+      </div>
+
+      <div className='boton__Principal'>
+        <a>
+
+          <i class="fa-solid fa-arrow-trend-up"></i>
+          <label className="Principal">
+            Popular
+          </label>
+        </a>
+      </div>
+
+      <hr className='separador'></hr>
+
+      <ComponentTEMAS/>
+
+      <hr className='separador'></hr>
+
+      <ComponentRECURSOS/>
+
+    <div className='derechosAutor'>
+      <a href="https://www.redditinc.com/">
+        <p>Reddit, Inc. © 2024. Todos los derechos reservados.</p>
+      </a>
+    </div>
+
+    </div>
   )
 }
